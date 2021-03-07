3b3809b9d7fc87ad9ea067d384e215f3aa1e984a
mwilco03 <36248105+mwilco03@users.noreply.github.com>
2021-03-07 13:01:23 -0500

    Update Repair-CoD.ps1

--- Repair-CoD.ps1
+++ Repair-CoD.ps1
@@ -1,6 +1,6 @@
 Function Repair-Cod{
     Stop-Process -Force -Name "Battle.net.exe"
-    $CoDs = "Call of Duty Black Ops Cold War Beta","Call of Duty Modern Warfare"
+    $CoDs = "Call of Duty Black Ops Cold War Beta","Call of Duty Modern Warfare","Call of Duty Black Ops Cold War"
     $Files = "Program Files","Users\$env:USERNAME\Documents"
     $Drives = (65..(65+25)).ForEach({ [char]$_ })
     $VPaths=[Collections.ArrayList]@()
